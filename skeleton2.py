d6eff57d681b30f4937737824d2e701cffe82387
Amrit Sreekumar <amrit.sreekumar@gmail.com>
2021-10-16 20:10:19 -0400

    callbacks, testing done

--- skeleton2.py
+++ skeleton2.py
@@ -7,31 +7,25 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras import layers
 import os
 import glob
+from sklearn.model_selection import train_test_split
 from tensorflow.python.keras import activations
 import pandas as pd
 from tensorflow.python.keras.layers.core import Flatten
-
-
 import numpy as np
 import matplotlib.pyplot as plt
-# from tqdm.notebook import tqdm
-
-import tensorflow
 from tensorflow.keras.layers import Conv2D, BatchNormalization, GlobalAveragePooling2D, \
 Dense, Input, Activation, MaxPool2D
 from tensorflow.keras import Model
-
-##import sklearn.metrics
-
 from numpy.random import default_rng
 
 
 
 class Datagen(tf.keras.utils.Sequence):
-  def __init__(self, list_IDs, labels, val, batch_size=32, n_classes=200, dim = (64,64), n_channels = 3, shuffle=True):
+  def __init__(self, list_IDs, label_ids, val_dict, val, batch_size=32, n_classes=200, dim = (64,64), n_channels = 3, shuffle=True):
     self.val = val
     self.dim = dim
-    self.labels = labels
+    self.label_ids = label_ids
+    self.val_dict = val_dict #to map between validation images and their corresponding classes
     self.list_IDs = list_IDs
     self.n_channels = n_channels
     self.batch_size = batch_size
@@ -66,27 +60,19 @@ class Datagen(tf.keras.utils.Sequence):
       for i, ID in enumerate(list_IDs_temp):
         # Store sample
         X[i,] = load_img('tiny-imagenet-200/val/images/' + ID)
-        # print("Val","i ", i, "ID",ID)
         # Store class
-        labelid = val_dict[ID]
-        new_label = label_ids[labelid]
-          #print(labelid)
-        #y[i] = self.labels[new_label]
+        labelid = self.val_dict[ID]
+        new_label = self.label_ids[labelid]
         y[i] = new_label
-        #print(tf.keras.utils.to_categorical(y, num_classes=self.n_classes))
       return X, tf.keras.utils.to_categorical(y, num_classes=self.n_classes)
 
     elif self.val == False: 
       for i, ID in enumerate(list_IDs_temp):
           # Store sample
           X[i,] = load_img('tiny-imagenet-200/train/' + ID + '/images/' + ID + '_' + str(i) +'.JPEG')
-          #print(X[i,].shape)
           # Store class
-          labelid = label_ids[ID]
-          #print(labelid)
-          #y[i] = self.labels[labelid]
+          labelid = self.label_ids[ID]
           y[i] = labelid
-          #print(tf.keras.utils.to_categorical(y, num_classes=self.n_classes))
       return X, tf.keras.utils.to_categorical(y, num_classes=self.n_classes)
       # return np.array(X), np.array(y)
 
@@ -98,7 +84,6 @@ label_ids = {}
 for i in range(len(class_ids)):
   label_ids[class_ids[i].split('\n')[0]] = i
 
-#print(label_ids)
 
 train_paths = glob.glob('tiny-imagenet-200/train/**/*.JPEG', recursive=True)
 train_images = []
@@ -106,49 +91,32 @@ for i in train_paths:
     train_images.append(os.path.basename(i).split('.')[0])
 
 train_labels = []
-newdictlabels = {}
-newlistlabels = []
-counter = 0
 for i in train_images:
     train_labels.append(i.split('_')[0])
-    k = i.split('_')[0]
-    if k not in newdictlabels:
-      newdictlabels[k] = counter
-      counter = counter + 1
-    newlistlabels.append(label_ids[k])
-
-print(len(newlistlabels))
-#print(len(newlistlabels))
 
 
 val_data = open('tiny-imagenet-200/val/val_annotations.txt', "r")
-#val_data.columns = ["image", "class", "x", "y", "w", "h"]
 val_data = val_data.readlines()
 validation_images = []
 validation_labels = []
 val_dict = {}
-counter2 = 0
 for i in range(len(val_data)):
   image_id = val_data[i].split('\t')[0]
   validation_images.append(image_id)
-
   image_label = val_data[i].split('\t')[1]
   validation_labels.append(image_label)
   val_dict[validation_images[i]] = validation_labels[i]
 
 
-new_val_list = []
-for i in validation_labels:
-  new_val_list.append(label_ids[i])
+
+X_train, X_test, y_train, y_test = train_test_split(train_labels, train_labels, test_size=0.2, random_state=42, shuffle=True)
 
 
-training_generator = Datagen(train_labels, newlistlabels, val = False)
-print(newlistlabels[:10])
-#new = iter(training_generator)
-##print(new)
-#change the below to above format train labels
-validation_generator = Datagen(validation_images, new_val_list, val = True)
-print(validation_images[:10])
+training_generator = Datagen(X_train, label_ids, val_dict = None, val = False)
+validation_generator = Datagen(X_test, label_ids, val_dict = None, val = False)
+test_generator = Datagen(validation_images, label_ids, val_dict, val = True)
+
+
 def model():
   input_img = Input(shape=(64, 64, 3))
   x = Conv2D(filters=32, kernel_size=(3, 3), strides=(1, 1), input_shape=(64,64,3), padding='same', activation=None)(input_img)
@@ -161,17 +129,31 @@ def model():
   x = Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), padding='same', activation=None)(x)
   x = BatchNormalization()(x)
   x = Activation('relu')(x)
-  x = GlobalAveragePooling2D()(x)
+  x = Flatten()(x)
   output = Dense(units=200, activation='softmax')(x)
 
   return Model(input_img, output)
 
+  
+
 model = model()
-model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate=0.01), loss = 'categorical_crossentropy', metrics = ["accuracy"],)
+model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate=0.001), loss = 'categorical_crossentropy', metrics = ["accuracy"],)
+
+checkpoint_filepath = 'checkpoint/'
+model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_filepath,
+    save_weights_only=False,
+    monitor='val_accuracy',
+    mode='max',
+    save_best_only=True)
 
 # Train model on dataset
 model.fit_generator(generator=training_generator,
                     validation_data=validation_generator,
                     use_multiprocessing=False,
-                    epochs = 10,
+                    epochs = 15,
+                    callbacks = [model_checkpoint_callback],
                     workers = 6)
+loss, acc = model.evaluate_generator(test_generator, steps=3, verbose=0)
+print(loss)
+print(acc)

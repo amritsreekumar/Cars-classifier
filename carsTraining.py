30b692d2b547296f3f16e9a1c134061b0a965e1b
Amrit Sreekumar <amrit.sreekumar@gmail.com>
2021-10-27 19:34:23 -0400

    Integrated Resnet

--- carsTraining.py
+++ carsTraining.py
@@ -7,6 +7,8 @@ import os
 import tensorflow as tf
 from Dataloader import DataGeneratorCars
 from scipy.io import loadmat
+import cubsArch2
+from cubsArch2 import cubs1cubs2res1
 
 if __name__ == "__main__":
     path_to_train_images = './cars_train'
@@ -26,23 +28,9 @@ if __name__ == "__main__":
     print( "Training labels size = ", len(labels))
 
 
-    model = models.Sequential()
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 3)))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(512, (3, 3), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Flatten())
-    #model.add(layers.Dense(512, activation='relu'))
-    model.add(layers.Dense(196, activation='softmax'))
+    model = cubs1cubs2res1(196)
 
-    model.summary()
-
+    model.build(input_shape=(100, 64, 64, 3))
     ##model compilation
     model.compile(loss='categorical_crossentropy',
                   optimizer=optimizers.Adam(lr=1e-4),
